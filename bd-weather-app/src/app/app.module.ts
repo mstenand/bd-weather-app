3d7764a5636c48aaace69d645d5429b98936d5e4
magnus-tagmus <mstenand@hotmail.com>
2023-10-07 20:58:12 +0200

    [BD-1] Created mock components and setup initial layout.

--- bd-weather-app/src/app/app.module.ts
+++ bd-weather-app/src/app/app.module.ts
@@ -5,10 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { StoreModule } from '@ngrx/store';
+import { HeaderComponent } from './header/header.component';
+import { OverviewComponent } from './overview/overview.component';
+import { GraphComponent } from './graph/graph.component';
+import { DailyComponent } from './daily/daily.component';
+import { WeeklyComponent } from './weekly/weekly.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    OverviewComponent,
+    GraphComponent,
+    DailyComponent,
+    WeeklyComponent
   ],
   imports: [
     BrowserModule,
